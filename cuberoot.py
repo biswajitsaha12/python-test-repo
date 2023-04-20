fb8b963b3d84e95f572ee0eb94e2755ab833bbd8
Biswajit Saha <biswajit.s4433@gmail.com>
2023-04-21 00:03:11 +0530

    adding cuberoot

--- cuberoot.py
+++ cuberoot.py
@@ -1,2 +1,28 @@
-n = int(input("Enter a number: "))
-print("Cube root of", n, "is", round(n**(1/3),2))
+try:
+  def cuberoot(num, step=0):
+    guess = 0
+    print(num)
+    while(abs((guess**3)-num) > 0.0001):
+      diff = abs((guess**3)-num)
+      print("diff: ", diff)
+      if (diff < num):
+        step = step + 1
+        print("+ve step: ",step)
+        guess += step
+        print("+ve guess :",guess)
+      else:
+        step = step - 1
+        print("-ve step: ",step)
+        guess -= step
+        print("-ve guess :",guess)
+    print("final diff: ",abs((guess**3)-num))    
+    return guess
+
+  num = int(input("enter a positive number: "))
+  if (num < 0):
+    print("Invalid input, expected a positive number")
+  else:
+    result = cuberoot(num)
+    print("cuberoot is: ", result)
+except ValueError:
+  print("Invalid input, expected a positive number")
