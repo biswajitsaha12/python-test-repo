9525eea65d78549d2928ae7034346aa5077e69ec
Biswajit Saha <biswajit.s4433@gmail.com>
2023-04-21 20:51:24 +0530

    modified the cuberoot program again

--- cuberoot.py
+++ cuberoot.py
@@ -1,28 +1,24 @@
 try:
-  def cuberoot(num, step=0):
+  def cuberoot(num, step=0.01):
     guess = 0
-    #print(num)
-    while(abs((guess**3)-num) > 0.0001):
-      diff = abs((guess**3)-num)
-      #print("diff: ", diff)
-      if (diff < num):
-        step = step + 1
-        #print("+ve step: ",step)
-        guess += step
-        #print("+ve guess :",guess)
-      else:
-        step = step - 1
-        #print("-ve step: ",step)
-        guess -= step
-        #print("-ve guess :",guess)
-    #print("final diff: ",abs((guess**3)-num))    
-    return guess
-
-  num = int(input("enter a positive number: "))
+    guess_count = 0
+    ep = 0.01
+    while(abs((guess**3)-num) >= ep) and (guess<=num):
+      guess += step
+      #print("guess:",guess)
+      guess_count+= 1
+      #print("guess count:",guess_count)
+      
+    #print("total guesses: ",guess_count)
+    if (abs((guess**3)-num) >= ep):
+      return print("failed to get cube root")
+    else:
+      return print("approx. cuberoot is: ",guess)
+      
+  num = float(input("enter a positive number to find it's cuberoot: "))
   if (num < 0):
     print("Invalid input, expected a positive number")
   else:
     result = cuberoot(num)
-    print("cuberoot is: ", result)
 except ValueError:
   print("Invalid input, expected a positive number")
